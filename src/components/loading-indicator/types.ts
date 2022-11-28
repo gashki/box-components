28e34133123c7e922ee3d07e73ca5f2257f3fb5c
gashki <20312509+gashki@users.noreply.github.com>
2022-11-27 19:03:35 -0800

    feat(loading-indicator): Create LoadingIndicator component

---
+++ src/components/loading-indicator/types.ts
@@ -0,0 +1,18 @@
+export enum LoadingIndicatorPosition {
+    BOTTOM = 'bottom',
+    MIDDLE = 'middle',
+    TOP = 'top',
+}
+
+export enum LoadingIndicatorSize {
+    LARGE = 'large',
+    MEDIUM = 'medium',
+    REGULAR = 'regular',
+    SMALL = 'small',
+}
+
+export enum LoadingIndicatorVisibility {
+    BLURRED = 'blurred',
+    HIDDEN = 'hidden',
+    VISIBLE = 'visible',
+}
